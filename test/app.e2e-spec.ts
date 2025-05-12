4226f4199b6f74aa1aa60fa7663680d3aebea455
Abdulrahman Fani <faniabdo99@gmail.com>
2025-05-13 00:54:30 +0300

    Updated the CI

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,25 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { AppService } from 'src/app.service';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+  let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
+      providers: [
+        AppService,
+        JwtService,
+        AuthGuard
+      ]
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('Root endpoint', () => {
+    it('/ (GET)', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/')
+        .expect(200);
+      
+      expect(response.text).toBe('Hello World!');
+    });
   });
 });
